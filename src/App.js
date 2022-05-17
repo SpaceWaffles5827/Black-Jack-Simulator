244ee9b2458cf7ae9b56db8441e297909f053dab
SpaceWaffles5827 <jackhartwig7@gmail.com>
2022-05-16 23:10:41 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { useState, useEffect } from "react"
 import BlackJack from "./BlackJack";
 import Home from "./Home";
 import "./App.css"
-
+import {Route ,BrowserRouter as Router, Routes} from "react-router-dom"
 
 function App() {
-
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home></Home>}/>
+            <Route path="/blackjack" element={<BlackJack></BlackJack>}/>
+          </Routes>
+        </Router>
+      </div>
   );
 }
 
