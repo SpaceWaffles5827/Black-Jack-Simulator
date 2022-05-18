1da0a3967df945978650f7f279378b0e1484d559
SpaceWaffles5827 <jackhartwig7@gmail.com>
2022-05-18 00:09:34 -0500

    added a second collum to the home page needs work

--- src/HomeContent.js
+++ src/HomeContent.js
@@ -1,4 +1,5 @@
 import "./HomeContent.css"
+import Project from "./Project";
 
 function HomeContent() {
     return (
@@ -7,23 +8,28 @@ function HomeContent() {
           <h1>Hey! I am Jack.</h1>
           <h1>A front end web developer.</h1>
         </div>
-        <div className="Projects">
-          <h2>&#60;Projects&#62;</h2>
-            <div className="project1">
-              <h1>Project 1</h1>
-            </div>
-            <div className="project1">
-              <h1>Project 2</h1>
-            </div>
-            <div className="project1">
-              <h1>Project 3</h1>
-            </div>
-            <div className="project1">
-              <h1>Project 4</h1>
-            </div>
+        <div className="Row2">
+          <div className="Projects">
             <h2>&#60;Projects&#62;</h2>
+              <div className="project1">
+                <h1>Project 1</h1>
+              </div>
+              <div className="project2">
+                <h1>Project 2</h1>
+              </div>
+              <div className="project3">
+                <h1>Project 3</h1>
+              </div>
+              <div className="project4">
+                <h1>Project 4</h1>
+              </div>
+              <h2>&#60;Projects&#62;</h2>
+            </div>
+            <div className="Things">
+            <h1>Row 2 idk</h1>
+          </div>
+          </div>
         </div>
-      </div>
     );
   }
   
---
+++ src/Project.js
@@ -0,0 +1,11 @@
+import "./Project.css"
+function Project({card}) {
+    return (
+      <div className="Project">
+          <h1>Project Here</h1>
+      </div>
+    );
+  }
+  
+  export default Project;
+  
